43e853a3f729bf70d58f48c7ec81e6cd4d9cbf92
JuliaTogni <julia.togni@sou.inteli.edu.br>
2022-05-04 11:40:47 -0300

    Resolução de problemas com algoritmos - semana 3

---
+++ 03_AUT_EST_ENTREGA/Semana 3/teste.js
@@ -0,0 +1,9 @@
+function getNome() {
+    velocidade = document.getElementById("velocidade").value;
+    alert(velocidade)
+    console.log(velocidade)
+    var tempo = velocidade/10
+    var altura = (velocidade * velocidade)/20
+    alert(tempo)
+    alert(altura)
+}
