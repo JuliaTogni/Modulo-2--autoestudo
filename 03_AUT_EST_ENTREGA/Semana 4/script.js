6b634169ebaf462e24d706a844222e0fda7bdacd
JuliaTogni <julia.togni@sou.inteli.edu.br>
2022-05-13 22:19:03 -0300

    Entrega Currículo com Js - semana 4

---
+++ 03_AUT_EST_ENTREGA/Semana 4/script.js
@@ -0,0 +1,37 @@
+//Há uma função para cada área do currículo, e foi utilizado `let` ao invés de var, para que o nome fosse mantido durante todo o projeto sem nenhum problema
+function showExperience() {
+    let divId = $("#expContent")
+
+    if (divId.attr("class") == "normal") {
+        divId.attr("class", "hiddenExpContent")
+
+    } else {
+        divId.attr("class", "normal")
+
+    }
+}
+
+function showStudies() {
+    let divId = $("#graContent") //Aqui entra a parte do JQuery para coletar o item com a id studiesContent
+  
+    // Algoritmo para mudar a classe da div no html, fazendo com que seu display esteja como none, ou normal
+    if (divId.attr("class") == "normal") {
+        divId.attr("class", "hiddenGraContent")
+
+    } else {
+        divId.attr("class", "normal")
+    }
+}
+
+function showLanguages() {
+    let divId = $("#idContent")
+
+    if (divId.attr("class") == "normal") {
+        divId.attr("class", "hiddenIdContent")
+
+        
+    } else {
+        divId.attr("class", "normal")
+
+    }
+}
