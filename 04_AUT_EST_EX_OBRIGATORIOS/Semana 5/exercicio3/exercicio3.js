9c7c1f4aa60715bbc90ad5d25d65bfe09ffc838b
JuliaTogni <julia.togni@sou.inteli.edu.br>
2022-05-20 11:11:32 -0300

    Semana 5

--- 04_AUT_EST_EX_OBRIGATORIOS/Semana 5/exercicio3/exercicio3.js
+++ 04_AUT_EST_EX_OBRIGATORIOS/Semana 5/exercicio3/exercicio3.js
@@ -1,30 +1,14 @@
-function ordenação(){
+function ordenaçao(){
+    let valores = [];
 
-    let input = $("#array").val()
-    let foco = $("#valor-foco").val()
+    let arrayInput = document.getElementById('array');
+    let valorFoco = document.getElementById('valor-foco');
+
+    arrayInput.value.split(',').forEach( array => {
+        valores.push(array.trim());
+    });
+
+    valores.sort((a , b) => a - b)
     
-    console.log(input)
-
-    input = input.split(",")
-
-    // input.map((val)=>{
-    //     array.push(Number(val))
-    // })
-
-
-    input.sort()
-
-
-
-
-
-    console.log(input)
-    let focoResult =input.indexOf(foco) + 1
-
-    if(focoResult == 0){
-        focoResult = "Esse valor não existe na sua sequência"
-    }
-
-    //alert('Ordenados de forma crescente ' + input + '<br> O número de' + foco + ' Número de ' + foco + ' está na ' + focoResult + ' º posição ')
-    let p = $("#resultado3").html(`Ordenados de forma crescente ${input} <br> O numero de ${foco} está na ${focoResult} º`)
+    console.log(valores)
 }
